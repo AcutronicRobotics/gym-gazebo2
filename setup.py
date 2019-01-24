3781bb7e50655f9c38fbdd39d21550e382743852
nzlz <nestor94gonzalez@gmail.com>
2019-01-24 15:53:52 +0700

    Update setup.py description field

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import sys, os.path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym-gazebo-ros2'))
 
 setup(name='gym-gazebo-ros2',
-      version='0.2.0',
+      version='0.0.1',
       packages=[package for package in find_packages()
                 if package.startswith('gym_gazebo_2')],
       install_requires=['gym>=0.2.3'],
-      description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using Gazebo and ROS 2.',
+      description='Gym-gazebo-2: A toolkit for developing and comparing your reinforcement learning agents using Gazebo and ROS 2.',
       url='https://github.com/erlerobot/gym-gazebo-ros2',
       author='Erle Robotics',
 )
