6d1be5f2f9c273869d58c3a884883b06f0bb2b8b
nzlz <nestor94gonzalez@gmail.com>
2019-01-28 15:09:41 +0700

    Change authory

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym-gazebo-ros2'))
 setup(name='gym-gazebo-ros2',
       version='0.0.1',
       packages=[package for package in find_packages()
-                if package.startswith('gym_gazebo_2')],
+                if package.startswith('gym_gazebo2')],
       install_requires=['gym>=0.2.3'],
       description='Gym-gazebo-2: A toolkit for developing and comparing your reinforcement learning agents using Gazebo and ROS 2.',
       url='https://github.com/erlerobot/gym-gazebo-ros2',
-      author='Erle Robotics',
+      author='Acutronic Robotics',
 )
