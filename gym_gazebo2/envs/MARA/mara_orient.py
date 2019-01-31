e3e9c6b3c643473aabc5c88e9ef7c67c413a6b81
yueerro <yue.trbj@gmail.com>
2019-01-31 12:23:02 +0100

    change orientation_scale place

--- gym_gazebo2/envs/MARA/mara_orient.py
+++ gym_gazebo2/envs/MARA/mara_orient.py
@@ -332,14 +332,14 @@ class MARAOrientEnv(gym.Env):
         if reward_dist < 0.005:
             reward = 1 + reward_dist # Make the reward increase as the distance decreases
             # Include orient reward if and only if it is close enough to the target
-            # Fetch the orientation of the end-effector which are from nr_dof:nr_dof+3 to nr_dof:nr_dof+6
-            reward_orient = ut_math.rmse_func(self.ob[self.scara_chain.getNrOfJoints()+3:(self.scara_chain.getNrOfJoints()+6)])
             orientation_scale = 0.1
-            if (reward_orient * orientation_scale) < 0.005:
-                reward = reward + reward_orient
+            # Fetch the orientation of the end-effector which are from nr_dof:nr_dof+3 to nr_dof:nr_dof+6
+            reward_orient = orientation_scale * ut_math.rmse_func(self.ob[self.scara_chain.getNrOfJoints()+3:(self.scara_chain.getNrOfJoints()+6)])
+            if reward_orient < 0.005:
+                reward = reward + reward_orient * 10
                 print("Reward is: ", reward)
             else:
-                reward = reward - reward_orient * orientation_scale
+                reward = reward - reward_orient
                 print("Reward (bad orient) is: ", reward)
         else:
             reward = -reward_dist
