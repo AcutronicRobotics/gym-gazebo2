5b149ec32d00c127d7b30ca02e3664e968c42a99
LanderU <lander.usategui@gmail.com>
2019-02-05 18:56:33 +0100

    gzclient.sh: fix docker container name

--- docker/gzclient.sh
+++ docker/gzclient.sh
@@ -3,6 +3,6 @@
 docker_path="$PWD"
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$docker_path/src/MARA
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$docker_path/src/MARA/mara_gazebo_plugins/build/
-export GAZEBO_MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gym2)
+export GAZEBO_MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gg2)
 export GAZEBO_MASTER_URI=$GAZEBO_MASTER_IP:11345
 gzclient
