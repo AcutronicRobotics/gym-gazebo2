562f2353cf5ea62a1e9c0ecc7fa369fbdfa03e35
LanderU <lander.usategui@gmail.com>
2019-02-05 18:28:56 +0100

    Update gzclient.sh

--- docker/gzclient.sh
+++ docker/gzclient.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 . /usr/share/gazebo/setup.sh
-docker_path="/home/nestor/ros_rl/environments/gym-gazebo-ros2/docker/"
+docker_path="$PWD"
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$docker_path/src/MARA
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$docker_path/src/MARA/mara_gazebo_plugins/build/
 export GAZEBO_MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gym2)
