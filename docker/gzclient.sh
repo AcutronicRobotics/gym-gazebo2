d7e238375889b85d54eeb0f278766129b4925452
Nestor Gonzalez <nestor94gonzalez@gmail.com>
2019-02-08 09:34:23 +0700

    Update gzclient.sh

--- docker/gzclient.sh
+++ docker/gzclient.sh
@@ -1,8 +1,7 @@
 #!bin/sh
-. /usr/share/gazebo/setup.sh
 docker_path="$PWD"
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$docker_path/src/MARA
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$docker_path/src/MARA/mara_gazebo_plugins/build/
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$docker_path/install/share
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$docker_path/install/lib
 export GAZEBO_MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' gg2)
 export GAZEBO_MASTER_URI=$GAZEBO_MASTER_IP:11345
 gzclient
