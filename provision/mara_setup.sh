4b9b3ecc46f477207a46a6ebb558530f7f1636ef
nzlz <nestor94gonzalez@gmail.com>
2019-02-08 09:24:49 +0700

    Fix mara_setup

--- provision/mara_setup.sh
+++ provision/mara_setup.sh
@@ -1,6 +1,5 @@
 source /opt/ros/crystal/setup.bash
 source ~/ros2_mara_ws/install/setup.bash
-source /usr/share/gazebo/setup.sh
 export PYTHONPATH=$PYTHONPATH:~/ros2_mara_ws/install/lib/python3/dist-packages
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_mara_ws/src/MARA
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:~/ros2_mara_ws/src/MARA/mara_gazebo_plugins/build/
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_mara_ws/install/share
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:~/ros2_mara_ws/install/lib
