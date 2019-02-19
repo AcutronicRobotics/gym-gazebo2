2b61a5efc889944d43c29fb223855a4b488f347b
nzlz <nestor94gonzalez@gmail.com>
2019-02-19 10:43:40 +0700

    Comply with new generate_launch_description

--- examples/MARA/debug/mara_debug.launch.py
+++ examples/MARA/debug/mara_debug.launch.py
@@ -1,4 +1,5 @@
 from gym_gazebo2.utils import ut_launch
+from ros2pkg.api import get_prefix_path
 
 def generate_launch_description():
     """
@@ -8,4 +9,6 @@ def generate_launch_description():
     real_speed = False
     multi_instance = False
     port = 11345
-    return ut_launch.generate_launch_description_mara(gzclient, real_speed, multi_instance, port)
+    urdf = get_prefix_path("mara_description") + "/share/mara_description/urdf/mara_robot_gripper_140.urdf"
+
+    return ut_launch.generate_launch_description_mara(gzclient, real_speed, multi_instance, port, urdf)
