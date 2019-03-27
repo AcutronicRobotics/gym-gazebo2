f287d366ef4c9a285367ef493fec2424df109005
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-03-27 13:20:32 +0100

    Remove unused function unit_vector()

--- gym_gazebo2/utils/general_utils.py
+++ gym_gazebo2/utils/general_utils.py
@@ -117,50 +117,6 @@ def getPosition(tf1, target, source, time):
 
     return position
 
-def unitVector(data, axis=None, out=None):
-    """Return ndarray normalized by length, i.e. Euclidean norm, along axis.
-
-    >>> v0 = np.random.random(3)
-    >>> v1 = unitVector(v0)
-    >>> np.allclose(v1, v0 / np.linalg.norm(v0))
-    True
-    >>> v0 = np.random.rand(5, 4, 3)
-    >>> v1 = unitVector(v0, axis=-1)
-    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=2)), 2)
-    >>> np.allclose(v1, v2)
-    True
-    >>> v1 = unitVector(v0, axis=1)
-    >>> v2 = v0 / np.expand_dims(np.sqrt(np.sum(v0*v0, axis=1)), 1)
-    >>> np.allclose(v1, v2)
-    True
-    >>> v1 = np.empty((5, 4, 3))
-    >>> unitVector(v0, axis=1, out=v1)
-    >>> np.allclose(v1, v2)
-    True
-    >>> list(unitVector([]))
-    []
-    >>> list(unitVector([1]))
-    [1.0]
-
-    """
-    if out is None:
-        data = np.array(data, dtype=np.float64, copy=True)
-        if data.ndim == 1:
-            data /= math.sqrt(np.dot(data, data))
-            return data
-    else:
-        if out is not data:
-            out[:] = np.array(data, copy=False)
-        data = out
-
-    length = np.atleast_1d(np.sum(data*data, axis))
-    np.sqrt(length, length)
-    if axis is not None:
-        length = np.expand_dims(length, axis)
-    data /= length
-    if out is None:
-        return data
-
 
 def getRotationMatrix(angle, direction, point=None):
     """Return matrix to rotate about axis defined by point and direction.
