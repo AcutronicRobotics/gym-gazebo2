a39b8cb0eec9ebde74a476083763e6640fc91527
rkojcev <rkojcev@gmail.com>
2019-05-20 12:39:11 +0200

    fix collision messages

--- gym_gazebo2/envs/MARA/mara_random_target.py
+++ gym_gazebo2/envs/MARA/mara_random_target.py
@@ -106,7 +106,7 @@ class MARARandomTargetEnv(gym.Env):
         # Set constants for links
         WORLD = 'world'
         TABLE = 'table'
-        BASE = 'base_link'
+        BASE = 'base_robot'
         MARA_MOTOR1_LINK = 'motor1_link'
         MARA_MOTOR2_LINK = 'motor2_link'
         MARA_MOTOR3_LINK = 'motor3_link'
@@ -117,7 +117,7 @@ class MARARandomTargetEnv(gym.Env):
 
         JOINT_ORDER = [MOTOR1_JOINT,MOTOR2_JOINT, MOTOR3_JOINT,
                         MOTOR4_JOINT, MOTOR5_JOINT, MOTOR6_JOINT]
-        LINK_NAMES = [ WORLD, TABLE, BASE,
+        LINK_NAMES = [ WORLD, BASE,
                         MARA_MOTOR1_LINK, MARA_MOTOR2_LINK,
                         MARA_MOTOR3_LINK, MARA_MOTOR4_LINK,
                         MARA_MOTOR5_LINK, MARA_MOTOR6_LINK, EE_LINK]
@@ -158,7 +158,7 @@ class MARARandomTargetEnv(gym.Env):
         # Initialize a KDL Jacobian solver from the chain.
         self.jacSolver = ChainJntToJacSolver(self.mara_chain)
 
-        self.obs_dim = self.numJoints + 6
+        self.obs_dim = self.numJoints + 9
 
         # # Here idially we should find the control range of the robot. Unfortunatelly in ROS/KDL there is nothing like this.
         # # I have tested this with the mujoco enviroment and the output is always same low[-1.,-1.], high[1.,1.]
@@ -289,8 +289,10 @@ class MARARandomTargetEnv(gym.Env):
         """
         Callback method for the subscriber of Collision data
         """
+        collision_messages = ["mara::base_robot::base_robot_collision", "ground_plane::link::collision"]
         if message.collision1_name != message.collision2_name:
-            self._collision_msg = message
+            if message.collision1_name not in collision_messages and message.collision2_name not in collision_messages:
+                self._collision_msg = message
 
     def set_episode_size(self, episode_size):
         self.max_episode_steps = episode_size
@@ -338,8 +340,8 @@ class MARARandomTargetEnv(gym.Env):
             # vector, typically denoted asrobot_id 'x'.
             state = np.r_[np.reshape(lastObservations, -1),
                           np.reshape(eePos_points, -1),
-                          np.reshape(eeVelocities, -1),]
-                          #np.reshape(self.targetPosition,-1)]
+                          np.reshape(eeVelocities, -1),
+                          np.reshape(self.targetPosition,-1)]
 
             return state
 
