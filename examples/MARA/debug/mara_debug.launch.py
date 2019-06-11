73b59ded53e5fc7a4319f08eb7ef9066fac30524
nzlz <nestor94gonzalez@gmail.com>
2019-06-11 16:56:32 +0700

    Fix debug file

--- examples/MARA/debug/mara_debug.launch.py
+++ examples/MARA/debug/mara_debug.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
     realSpeed = False
     multiInstance = False
     port = 11345
-    urdf = "reinforcement_learning/mara_robot_gripper_140_train.urdf"
+    urdf = "reinforcement_learning/mara_robot_train.urdf"
     urdfPath = get_prefix_path("mara_description") + "/share/mara_description/urdf/" + urdf
 
     return ut_launch.generateLaunchDescriptionMara(gzclient, realSpeed, multiInstance, port, urdfPath)
