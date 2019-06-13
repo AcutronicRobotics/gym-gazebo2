ab5fbc6a07678e3161ab7f5a53fbd5a7a9844aee
rkojcev <rkojcev@gmail.com>
2019-06-13 12:59:55 +0200

    update provision

--- provision/mara_setup.sh
+++ provision/mara_setup.sh
@@ -1,6 +1,7 @@
 source /opt/ros/dashing/setup.bash
 source ~/ros2_mara_ws/install/setup.bash
 source /usr/share/gazebo-9/setup.sh
+export RMW_IMPLEMENTATION=rmw_opensplice_cpp
 export PYTHONPATH=$PYTHONPATH:~/ros2_mara_ws/install/lib/python3/dist-packages
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ros2_mara_ws/install/share
 export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:~/ros2_mara_ws/install/lib
